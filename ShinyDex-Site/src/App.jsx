77e3a51089bbb56fc4e2c8b9344e634bcdf57101
FaudaTV <shadowgames1547@gmail.com>
2026-03-31 20:09:50 +0200

    Add gen 8 data and sprites

--- ShinyDex-Site/src/App.jsx
+++ ShinyDex-Site/src/App.jsx
@@ -11,8 +11,9 @@ import gen4 from './data/gen4.json';
 import gen5 from './data/gen5.json';
 import gen6 from './data/gen6.json';
 import gen7 from './data/gen7.json';
+import gen8 from './data/gen8.json';
 
-const allPokemonData = [...gen1, ...gen2, ...gen3, ...gen4, ...gen5, ...gen6, ...gen7];
+const allPokemonData = [...gen1, ...gen2, ...gen3, ...gen4, ...gen5, ...gen6, ...gen7, ...gen8];
 
 function App() {
   // --- ÉTATS ---
